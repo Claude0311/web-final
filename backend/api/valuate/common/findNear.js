e2d6f6ebc7acb865e4f3e425c9eb993214c4e96d
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2021-01-13 20:28:26 +0800

    fisrt distance as filter

--- backend/api/valuate/common/findNear.js
+++ backend/api/valuate/common/findNear.js
@@ -5,16 +5,23 @@ import House from '../../../model/House'
 import House_detail from '../../../model/House_detail'
 import Valuate from '../../../model/Valuate'
 import { meter2Lat, meter2Lng } from '../../../util/unitTrans'
+import Score from '../../../model/Score'
 
 const findNear = async (req,res,next) => {
     const {coordinate:{lat,lng},buildingType,floor,age} = req.valuate
     console.log(lat,lng,buildingType,age,floor)
+    const farestDis = await Score.find({className:'Distance'}).sort({priority:1})
+    let query = {}
+    if(farestDis){
+        const dis = farestDis[0].param
+        console.log({dis})
+        query = {
+            'coordinate.lat':{$gt:lat-meter2Lat*dis,$lt:lat+meter2Lat*dis},
+            'coordinate.lng':{$gt:lng-meter2Lng*dis,$lt:lng+meter2Lng*dis}
+        }
+    }
     const nears = await House
-        .find({
-            // buildingType,
-            // 'coordinate.lat':{$gt:lat-meter2Lat*1000,$lt:lat+meter2Lat*1000},
-            // 'coordinate.lng':{$gt:lng-meter2Lng*1000,$lt:lng+meter2Lng*1000}
-        }).sort({ _id: -1 }).populate('detail') //.limit(100)
+        .find(query).sort({ _id: -1 }).populate('detail') //.limit(100)
         .catch(dbCatch)
     const scoreInput = {age,coordinate:{lat,lng},floor}
     req.nears = nears
