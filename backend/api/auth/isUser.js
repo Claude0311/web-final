dc74c326af66942a5185d172e1a0e09a7f163cd0
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2021-01-17 15:39:15 +0800

    isAuth block

--- backend/api/auth/isUser.js
+++ backend/api/auth/isUser.js
@@ -4,7 +4,7 @@ const isUser = (req,res,next) => {
     const {user} = req.session
     // console.log('yuser',req.session)
     // console.log('user',user)
-    if(!user) throw new ErrorHandler(404,'not login')
+    if(!user&&process.env.USE_AUTH==="true") throw new ErrorHandler(404,'not login')
     next()
 }
 
