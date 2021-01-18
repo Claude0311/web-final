f92fdd42a60c3a5823717967e04fa12e09b28d93
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2021-01-18 11:52:00 +0800

    unitTrans

--- backend/util/unitTrans.js
+++ backend/util/unitTrans.js
@@ -1,5 +1,5 @@
-const meter2Lat = 1/1.1131955
-const meter2Lng = 1/1.0089
+const meter2Lat = 0.00001/1.1131955
+const meter2Lng = 0.00001/1.0089
 export {
     meter2Lat,
     meter2Lng
--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -103,7 +103,7 @@ export const init = async () => {
     const dbCatch = e=>{console.log('myError:',e?.response?.data?.msg)}
     // const {data:{user,auth}} = await instance.post('/login',{user:'b07901029',password:'123'}).catch(dbCatch)
     // console.log(user,auth)
-    // const {data} = await instance.get('/geoCode',{params:{address:''}}).catch(dbCatch)
+    // const {data} = await instance.get('/houses',{params:{neighbor:{center:{lat:25,lng:121.5},distance:800}}}).catch(dbCatch)
     // console.log(data)
 }
 
