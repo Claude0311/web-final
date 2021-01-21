3eeb5902bd1265642680312de979588d77e0f293
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2021-01-21 12:25:47 +0800

    uri

--- backend/model/db.js
+++ backend/model/db.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose'
+import env from 'dotenv'
+env.config('../../../')
 
-const DB_URL1 = 'mongodb+srv://ChunFuBank:bankpassword@cluster0.nqvst.mongodb.net/Bank?retryWrites=true&w=majority'
+const DB_URL1 = ''//'mongodb+srv://ChunFuBank:bankpassword@cluster0.nqvst.mongodb.net/Bank?retryWrites=true&w=majority'
 const DB_URL = process.env.MONGO_URI||DB_URL1
+console.log(`url:${DB_URL}`)
 
 mongoose.set('useUnifiedTopology', true)
 mongoose.set('useCreateIndex', true)
